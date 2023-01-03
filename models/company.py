014a276523bcd8307bc7a60191b2e636a23a721e
ChaoqiYin <305111632@qq.com>
2023-01-03 16:16:31 +0800

    docs: 优化md文档，去除test代码

--- models/company.py
+++ models/company.py
@@ -15,7 +15,8 @@ class Company(models.Model):
 
     def on_oa_callback(self, xml_tree):
         """
-        callback when wechat enterprise oa event, you can override this method to do something
+        callback when wechat enterprise oa event, you can override this method to do something, reference url:
+        https://developer.work.weixin.qq.com/document/path/90240#%E5%AE%A1%E6%89%B9%E7%8A%B6%E6%80%81%E9%80%9A%E7%9F%A5%E4%BA%8B%E4%BB%B6
         :param xml_tree: callback info by xml, you can get attribute by xml_tree.find('xxx').find('xxx').text
         :return:
         """
